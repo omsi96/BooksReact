6b91472d49aecdd5b42ed8f1770ce0695a2c9e2c
Omar Alibrahim <omsi96@gmail.com>
2021-07-28 14:21:58 +0300

    ADDING YUM COOKIESS

--- books-react/src/App.js
+++ books-react/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import MainRoute from "./pages/MainRoute";
+import Cookies from "js-cookie";
 
 function App() {
+  Cookies.set("FLAG", "cookie{124}");
+  console.log({ cookies: document.cookie });
   return (
     <div>
       {/* <HomePage /> */}
